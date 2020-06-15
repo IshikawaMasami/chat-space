8591d27091272bb7acc74b6e2608423411ebbfa8
IshikawaMasami <masa2019@i.softbank.jp>
2020-06-15 13:17:01 +0900

    API inplemented
    lesson10 API is inplemented

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -38,7 +38,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
-  process resize_to_fit: [100, 100]
+  process resize_to_fit: [300, 300]
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
